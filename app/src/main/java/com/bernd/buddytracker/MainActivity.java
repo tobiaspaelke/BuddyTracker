aa87e674b914f44b38175d88fe459c079fd77fd7
steinstein <alexx.bereza@gmail.com>
2015-01-19 11:46:18 +0100

    testcommit

--- app/src/main/java/com/bernd/buddytracker/MainActivity.java
+++ app/src/main/java/com/bernd/buddytracker/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-
+//test commit
 public class MainActivity extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
